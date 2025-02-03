a5b0e7b91ad0334dcb5fc461ad483af8c50be0fd
leandroxlex1x <leandroxlex1x@gmail.com>
2025-02-03 02:53:30 +0000

    part1

--- app.js
+++ app.js
@@ -2,25 +2,24 @@
 let amigos = [];
 
 function agregarAmigo() {
-    const nombre = document.getElementById("amigo");
+    const nombre = document.getElementById("amigo").value;
     if (nombre === "") {
         alert("Por favor, inserte un nombre."); // Muestra una alerta si el campo está vacío
-      }
-
-      amigos.push(nombre); // Agrega el nombre al array de amigos
+    }
     
-      document.querySelector(".input-name").value = ""; // Limpia el campo de entrada
-      actualizarLista(); // Actualiza la lista de amigos en la interfaz de usuario (ver función abajo)    
-      return;
+    amigos.push(nombre); // Agrega el nombre al array de amigos
+    document.querySelector(".input-name").value = ""; // Limpia el campo de entrada
+    actualizarLista(); // Actualiza la lista de amigos en la interfaz de usuario (ver función abajo)    
+    return;
     }
 
     
     function actualizarLista() {
-        const listaAmigos = document.getElementById("listaAmigos");
-        listaAmigos.innerHTML = ""; // Limpia la lista antes de actualizarla
+       const listaAmigos = document.getElementById("listaAmigos");
+       listaAmigos.innerHTML = ""; // Limpia la lista antes de actualizarla
 
     amigos.forEach(amigo => {
-        const elementoLista = document.getElementById("listaAmigos");
+        const elementoLista = document.createElement("li");
         elementoLista.textContent = amigo;
         listaAmigos.appendChild(elementoLista);
     });
